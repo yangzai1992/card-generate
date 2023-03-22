fd40eb4d405b38b413f4473ea21e46e9fa9e2f6b
fengyangyang <ex-fengyangyang001@pingan.com.cn>
2023-03-22 18:30:38 +0800

    打包命令换行处理

--- package.bat
+++ package.bat
@@ -1,1 +1,3 @@
-mvn javafx:jlink && jpackage --name cardGenerate --type app-image -m com.yang.javafx/com.yang.javafx.MainApplication --runtime-image ./target/app/  --icon ./doc/img/cardGenerate.ico && rmdir /s/q target
+mvn javafx:jlink && ^
+jpackage --name cardGenerate --type app-image -m com.yang.javafx/com.yang.javafx.MainApplication --runtime-image ./target/app/  --icon ./doc/img/cardGenerate.ico && ^
+rmdir /s/q target
